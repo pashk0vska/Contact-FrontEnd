82730794874195684e3f71aa698148084ec3f2e4
fukkut <svyatoslav.luchko@kpk-lp.com.ua>
2025-11-12 09:00:45 +0200

    Файли конфігурації демо-версії проекту
    - Підключення бази даних і передачу даних у реальному часі
    - Основні файли налаштувань (main.js, package.json, icons тощо)

---
+++ main.js
@@ -0,0 +1,18 @@
+﻿const { app, BrowserWindow } = require('electron');
+
+function createWindow(){
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    show: false,
+    autoHideMenuBar: true
+  });
+  win.loadFile('renderer/index.html');
+  win.once('ready-to-show', () => {
+    win.maximize();   // розгортаємо
+    win.show();       // показуємо без “мигання” розміру
+  });
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => { if (process.platform !== 'darwin') app.quit(); });
